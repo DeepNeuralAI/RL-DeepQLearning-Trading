0173710ec549b046cd9e967be18fc47019ee9320
Aaron Mendonsa <amendonsa@Aarons-MacBook-Air.local>
2020-01-21 15:30:37 -0800

    Adding simple heuristic trader

---
+++ algorithmic_trader_with_deepRL/utils/heuristicTrader.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from util import data_df
+
+class HeuristicTrader():
+  def __init__(self, symbol, start_date, end_date, window = 7, max_shares = 100):
+    self.symbol = symbol
+    self.start_date, self.end_date = start_date, end_date
+    self.window, self.max_shares = window, max_shares
+    self.data = self.get_data()
+    self.policy = self.strategy()
+    self.shares = self.shares_df()
+    self.trades = self.trades_df()
+
+  def get_data(self):
+    data = data_df(self.symbol, n_period = self.window)
+    date_range = pd.date_range(start = self.start_date, end = self.end_date)
+    data_ = data.loc[date_range].dropna(subset=[self.symbol])
+    return data_
+
+  def strategy(self):
+    tmp_df = pd.DataFrame(index = self.data.index)
+    tmp_df['indicator'] = np.zeros(self.data.shape[0])
+
+    buy = (self.data['vol_bbp'] < 0.5) & (self.data['trend_rsi'] <= 30) & \
+        (self.data['trend_p2sma'] <= self.data['trend_p2sma'].quantile(0.25))
+    sell = (self.data['vol_bbp'] >= 0.5) & (self.data['trend_rsi'] > 50) & \
+        (self.data['trend_p2sma'] >= self.data['trend_p2sma'].quantile(.75))
+
+    tmp_df['indicator'][buy] = 1
+    tmp_df['indicator'][sell] = -1
+    return tmp_df
+
+  def shares_df(self):
+    shares = []
+    net_holdings = 0
+
+    for index, row in self.policy.iterrows():
+      buy = row['indicator'] > 0
+      sell = row['indicator'] < 0
+
+      if buy and net_holdings == 0:
+          shares.append(self.max_shares)
+          net_holdings += self.max_shares
+      elif buy and net_holdings == -self.max_shares:
+          shares.append(self.max_shares * 2)
+          net_holdings += self.max_shares * 2
+      elif sell and net_holdings == 0:
+          shares.append(-self.max_shares)
+          net_holdings += -self.max_shares
+      elif sell and net_holdings == self.max_shares:
+          shares.append(-self.max_shares * 2)
+          net_holdings += -self.max_shares * 2
+      else:
+          shares.append(0)
+    return shares
+
+  def trades_df(self):
+    trades = pd.DataFrame(0.0, columns=[self.symbol, 'Cash'], index=self.data.index)
+    current_value = self.shares * (-self.data[self.symbol])
+    trades.loc[self.data.index, self.symbol] = self.shares
+    trades.loc[self.data.index, 'Cash'] += current_value
+    return trades
--- algorithmic_trader_with_deepRL/utils/util.py
+++ algorithmic_trader_with_deepRL/utils/util.py
@@ -1,17 +1,18 @@
 from technical_indicators import *
 from get_data import load_data, get_path
 
-def indicators_dict(symbol, prices, volume):
+def indicators_dict(symbol, prices, volume, window = 7):
   return {
     f'{symbol}': prices,
-    'trend_rsi': relative_strength_index(prices),
-    'mom_moms': momentum(prices),
-    'trend_stok': stochastic_oscillator_k(prices),
-    'trend_stod': stochastic_oscillator_d(prices),
+    f'{symbol}_norm': normalize(prices),
+    'trend_rsi': relative_strength_index(prices, window),
+    'mom_moms': momentum(prices, window),
+    'trend_stok': stochastic_oscillator_k(prices, window),
+    'trend_stod': stochastic_oscillator_d(prices, window),
     'volume_obv': on_balance_volume(prices, volume),
-    'trend_sma': simple_moving_average(prices),
-    'trend_p2sma': price_to_sma(prices, simple_moving_average(prices)),
-    'trend_ema': exponential_moving_average(prices),
+    'trend_sma': simple_moving_average(prices, window),
+    'trend_p2sma': price_to_sma(prices, simple_moving_average(prices, window)),
+    'trend_ema': exponential_moving_average(prices, window),
     'trend_p2ema': price_to_ema(prices, exponential_moving_average(prices)),
     'trend_macd': moving_average_convergence_divergence(prices),
     'vol_bbl': bollinger_bands(prices, simple_moving_average(prices))[0],
@@ -26,7 +27,7 @@ def create_df(data, index):
 
 def data_df(symbol, n_period = 7, fillna = True):
   data = load_data(symbol)
-  inds = indicators_dict(symbol, normalize(data[symbol]), data.volume)
+  inds = indicators_dict(symbol, data[symbol], data.volume, window = n_period)
   df = create_df(inds, data.index)
 
   high, low, close, volume = data.high, data.low, data.close, data.volume
